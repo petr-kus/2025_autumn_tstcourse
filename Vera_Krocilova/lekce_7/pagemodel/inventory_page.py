ab5a751cf7d80a08ef6582b89aa8aed2afcd2ea9
vera-krocilova <vera.krocilova@gmail.com>
2025-11-28 23:11:05 +0100

    oprava - lekce 7 - test uz nepada

--- Vera_Krocilova/lekce_7/pagemodel/inventory_page.py
+++ Vera_Krocilova/lekce_7/pagemodel/inventory_page.py
@@ -8,10 +8,10 @@ class InventoryPage():
     URL = "https://www.saucedemo.com/inventory.html"
 
     PRODUCTS = (By.CLASS_NAME, "inventory_item")
-    ADD_TO_CART_BUTTON = (By.CLASS_NAME, "btn_inventory")
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".pricebar button")
     BADGES_CART = (By.CLASS_NAME, "shopping_cart_badge")
-    IMAGE_INVENTORY_ITEM = (By.CLASS_NAME, "inventory_item_img")
-    INVENTORY_ITEM_NAME = (By.CLASS_NAME, "inventory_item_name")
+    PRODUCT_IMAGE = (By.CSS_SELECTOR, ".inventory_item_img img")
+    PRODUCT_NAME = (By.CLASS_NAME, "inventory_item_name")
     CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
 
 
@@ -25,17 +25,16 @@ class InventoryPage():
 
 
     def choose_random_product(self, products):
-        random_product = random.choice(products)
+        random_product = random.choice(products)     
         return random_product
 
 
     def add_to_cart(self, random_product):
-        product_name = random_product.find_element(*self.INVENTORY_ITEM_NAME).text
+        name_product = random_product.find_element(*self.PRODUCT_NAME).text
         add_button = random_product.find_element(*self.ADD_TO_CART_BUTTON)
         add_button.click()
-        self.last_added_product_name = product_name
-        return random_product
-
+        return name_product
+        
 
     def cart_count(self):
         cart_badge = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.BADGES_CART))
@@ -50,12 +49,26 @@ class InventoryPage():
         cart_icon.click()
         
 
-    def display_image_item(self, random_product):
-        image_product = random_product.find_element(*self.IMAGE_INVENTORY_ITEM)
-        return image_product.get_attribute("src")
+    def click_product_image(self, random_product):
+        product_image = random_product.find_element(*self.PRODUCT_IMAGE)
+        image_src = product_image.get_attribute("src")
+        product_image.click()
+        return image_src
+    
+    def get_detail_image_src(self):
+        detail_image = self.driver.find_element(By.CLASS_NAME, "inventory_details_img")
+        return detail_image.get_attribute("src")
 
 
-    def display_details_item(self, random_product):
-        name_product = random_product.find_element(*self.INVENTORY_ITEM_NAME)
-        return name_product.text
+    def click_product_name(self, random_product):
+        name_product = random_product.find_element(*self.PRODUCT_NAME)
+        name_product_text = name_product.text
+        name_product.click()
+        return name_product_text
+    
+    def get_detail_name(self):
+        detail_name = self.driver.find_element(By.CLASS_NAME, "inventory_details_name")
+        return detail_name.text
+        
+        
 
